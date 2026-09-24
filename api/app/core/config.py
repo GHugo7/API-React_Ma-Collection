1a48fbd034332f0402ff455693118ecfc60d4462
Hugo Giordano <hugo.giordano@ynov.com>
2026-09-24 22:58:49 +0200

    Configure Settings avec pydantic-settings pour charger les variables d'environnement

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -1,10 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-secret_key = os.getenv("SECRET_KEY")
 
 class Settings(BaseSettings):
-    
+    # parametre de comment charger le fichier 
+    # env_file : charger le fichier env sinon il prend les valeur du systeme
+    # extra="ignore" : ignore les variables non charge, inutile
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    secret_key: str
+    algorithm: str = "HS256"
+    token_expire_min: int = 30
+    database_url: str
+    cors_origin: str = "http://localhost:5173"
+
+
+settings = Settings()
