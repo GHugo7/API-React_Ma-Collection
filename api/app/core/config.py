eac071976692135cdd3680137c3f6967b6e6bcb0
Hugo <hugo.giordano@ynov.com>
2026-09-24 21:02:32 +0200

    Pose l'arborescence de l'API FastAPI (core, db, models, routers, schemas)

---
+++ api/app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
+
+class Settings(BaseSettings):
+    
